d6cf90309a4ef685f4ad1249132be078cb08bf93
Thomas LEVIANDIER <tleviandier-ext@deezer.com>
2016-10-03 16:47:20 +0200

    customer custom fields

--- src/Cadulis/Sdk/Model/Customer.php
+++ src/Cadulis/Sdk/Model/Customer.php
@@ -20,4 +20,37 @@ class Customer extends AbstractModel
     public $email;
     protected $_properties = array('id', 'reference', 'type', 'name', 'address', 'address_additional', 'phone', 'mobile', 'comment', 'email');
 
+    /**
+     * @var CustomerCustomFields
+     */
+    public $custom_fields;
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $return = parent::toArray();
+        if (!empty($this->custom_fields)) {
+            if (!($this->custom_fields instanceof CustomerCustomFields)) {
+                throw new \Cadulis\Sdk\Exception('invalid custom_fields instance (must be instanceof \Cadulis\Sdk\CustomerCustomFields');
+            }
+            $return['custom_fields'] = $this->custom_fields->toArray();
+        }
+
+        return $return;
+    }
+
+    public function hydrate(array $data = [])
+    {
+        parent::hydrate($data);
+        if (isset($data['custom_fields'])) {
+            $this->custom_fields = new CustomerCustomFields;
+            if (!is_array($data['custom_fields'])) {
+                throw new \Exception('Invalid parameter "custom_fields"');
+            }
+            $this->custom_fields->hydrate($data['custom_fields']);
+        }
+    }
+
 }
